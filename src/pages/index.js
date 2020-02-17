89b4066e1ec6305e38a130b8c43ad9b4558f8a43
aldo-jr <aldo.dfernandes@gmail.com>
2020-02-17 20:19:59 -0300

    get list of post details on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,70 @@
+import { Link, graphql, useStaticQuery } from 'gatsby'
+
 import Layout from 'components/layout'
 import React from 'react'
 import SEO from 'components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello World</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
+        edges {
+          node {
+            name
+            publishDate(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-BR")
+            slug
+            mainImage {
+              title
+              file {
+                url
+              }
+              description
+            }
+            category {
+              name
+              slug
+              parentCategory {
+                name
+                slug
+              }
+            }
+            description {
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const posts = data.allContentfulBlogPost.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {posts.map(edge => {
+        const post = edge.node
+        const { category, mainImage } = post
+        const { parentCategory } = category
+
+        return (
+          <article key={post.id}>
+            <Link to={`/blog/${post.slug}`}>
+              <h2>{post.name}</h2>
+            </Link>
+            <p>{post.publishDate}</p>
+            {parentCategory && parentCategory.name && (
+              <p>{parentCategory.name}</p>
+            )}
+            <p>{category.name}</p>
+            <img src={mainImage.file.url} alt={mainImage.title} />
+
+            <p>{post.description.description}</p>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default IndexPage
